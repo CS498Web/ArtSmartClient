58cd30f605904e95f4bfb0dca88d4a32ad2bfdef
Hubert Theodore <hubert@yup.com>
2016-05-02 11:19:03 -0500

    connected to the backend

---
+++ app/scripts/services/artwork-service.js
@@ -0,0 +1,30 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name anotareApp.factory:Album
+ */ 
+
+angular.module('anotareApp')
+  .factory('ArtworkService', function ($http) {
+    var baseUrl = "http://localhost:4000/api"
+    var formHeader = {headers:{ 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}}
+    // delete $http.defaults.headers.post['Content-type'];
+    // delete $http.defaults.headers.delete['Content-type'];
+    return {
+        getOne : function(artwork_id) {
+            return $http.get(baseUrl + '/artworks/' + artwork_id);
+        },
+        postOne : function(data) {
+            return $http.post(baseUrl+'/artworks', $.param(data), formHeader);
+        },
+        deleteOne : function(artwork_id) {
+            return $http.delete(baseUrl+'/artworks/' + artwork_id);
+        },
+        putOne : function(artwork_id, data) {
+            return $http.put(baseUrl+'/artworks/' + artwork_id, $.param(data), formHeader);
+        },
+        deleteAnnotation: function(artwork_id, annotation_id) {
+            return $http.delete(baseUrl + '/artworks/' + artwork_id + '/annotations/' + annotation_id);
+        }
+    }
+});
